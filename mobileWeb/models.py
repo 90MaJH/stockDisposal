5ef733f27952e8bce0d8183ed41b25fc94548190
90majh <90majh@gmail.com>
2020-01-20 20:55:42 +0900

    make models of mart, item

--- mobileWeb/models.py
+++ mobileWeb/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+
+class mart(models.Model):
+    name = models.CharField(blank=False, max_length=20)
+    address = models.TextField(blank=False)
+    tell = models.CharField(blank=True, max_length=12)
+    phone = models.CharField(blank=True, max_length=11)
+    delete_yn = models.CharField(blank=False, default="N", max_length=1)
+    ins_dttm = models.DateTimeField(blank=False, auto_now_add=True)
+    ins_user = models.CharField(blank=False, max_length=20, default='ADMIN')
+    upt_dttm = models.DateTimeField(blank=False, auto_now=True)
+    upt_user = models.CharField(blank=False, max_length=20, default='ADMIN')
+
+class item(models.Model):
+    mart_id = models.ForeignKey('mart', models.DO_NOTHING)
+    seq = models.IntegerField(blank=False)
+    name = models.CharField(blank=False, max_length=20)
+    price = models.IntegerField(blank=False)
+    expirationDate = models.DateField(blank=False)
+    stockYn = models.CharField(blank=False, max_length=1, default='Y')
+    delete_yn = models.CharField(blank=False, default="N", max_length=1)
+    ins_dttm = models.DateTimeField(blank=False, auto_now_add=True)
+    ins_user = models.CharField(blank=False, max_length=20, default='ADMIN')
+    upt_dttm = models.DateTimeField(blank=False, auto_now=True)
+    upt_user = models.CharField(blank=False, max_length=20, default='ADMIN')
+
+    class Meta:
+        unique_together = (
+            ('mart_id', 'seq')
+        )
