ef94b337c546df3f7c9c6b1a51b7a7f2ecabddf4
90majh <90majh@gmail.com>
2020-02-19 23:04:13 +0900

    add delete page

--- mobileWeb/urls.py
+++ mobileWeb/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('index', views.index, name='index'),
     path('registerMart', views.registerMart, name='registerMart'),
     path('registerItem', views.registerItem, name='registerName'),
+    path('delete', views.delete, name='delete'),
+    path('deleteMart', views.deleteMart, name='deleteMart'),
+    path('deleteItem', views.deleteItem, name='deleteItem'),
 ]
--- mobileWeb/views.py
+++ mobileWeb/views.py
@@ -1,33 +1,39 @@
 from django.shortcuts import render
 from .forms import *
 from datetime import datetime
+from django.http import HttpResponse
+
 
 # Create your views here.
 
 def index(request):
     try:
-        marts = MartModel.objects.all().values('id', 'name', 'imageFileNo', 'xPosition', 'yPosition')
-        items = ItemModel.objects.filter(stockYn__exact='Y').filter(expirationDate__gte=datetime.now()).values('mart', 'name', 'price', 'expirationDate').order_by('mart_id', 'seq')
+        marts = MartModel.objects.filter(use_yn__exact='Y').values('id', 'name', 'imageFileNo', 'xPosition',
+                                                                   'yPosition')
+        items = ItemModel.objects.filter(stockYn__exact='Y').filter(use_yn__exact='Y').filter(
+            expirationDate__gte=datetime.now()).values('mart', 'name', 'price', 'expirationDate').order_by('mart_id',
+                                                                                                           'seq')
 
-        return render(request, 'mobileWeb/index/index.html', {'marts':marts, 'items':items})
+        return render(request, 'mobileWeb/index/index.html', {'marts': marts, 'items': items})
     except Exception as ex:
         print('Error occured : ', ex)
 
+
 def registerMart(request):
     try:
-        if request.method == 'POST' :
+        if request.method == 'POST':
             form = MartForm(request.POST)
             if form.is_valid():
                 form.save()
                 return render(request, 'mobileWeb/index/index.html')
-        else :
+        else:
             form = MartForm()
-            return render(request, 'mobileWeb/admin/register_mart.html', {'form':form})
+            return render(request, 'mobileWeb/admin/register_mart.html', {'form': form})
     except Exception as ex:
         print('Error occured : ', ex)
 
+
 def registerItem(request):
-    print(request)
     try:
         if request.method == 'POST':
             form = ItemForm(request.POST)
@@ -35,15 +41,49 @@ def registerItem(request):
                 mart = form.cleaned_data['mart']
                 seq = ItemModel.objects.filter(mart__exact=mart).values('seq').order_by('-seq')[:1]
                 if seq:
-                    seq = seq[0]['seq']+1
+                    seq = seq[0]['seq'] + 1
                 else:
                     seq = 1
-                item = ItemModel(mart=mart, seq=seq, name=form.cleaned_data['name'], price=form.cleaned_data['price'], expirationDate=form.cleaned_data['expirationDate'])
+                item = ItemModel(mart=mart, seq=seq, name=form.cleaned_data['name'], price=form.cleaned_data['price'],
+                                 expirationDate=form.cleaned_data['expirationDate'])
                 item.save()
                 form = ItemForm()
-                return render(request, 'mobileWeb/admin/register_item.html', {'form':form})
+                return render(request, 'mobileWeb/admin/register_item.html', {'form': form})
         else:
             form = ItemForm()
-            return render(request, 'mobileWeb/admin/register_item.html', {'form':form})
+            return render(request, 'mobileWeb/admin/register_item.html', {'form': form})
     except Exception as ex:
-        print(': Error occured : ', ex)
+        print('Error occured : ', ex)
+
+
+def delete(request):
+    try:
+        marts = MartModel.objects.filter(use_yn__exact='Y').values('id', 'name', 'imageFileNo', 'xPosition',
+                                                                   'yPosition')
+        items = ItemModel.objects.filter(stockYn__exact='Y').filter(use_yn__exact='Y').filter(
+            expirationDate__gte=datetime.now()).values('id', 'mart', 'name', 'price', 'expirationDate').order_by('mart_id',
+                                                                                                           'seq')
+
+        return render(request, 'mobileWeb/admin/delete.html', {'marts': marts, 'items': items})
+    except Exception as ex:
+        print('Error occured : ', ex)
+
+
+def deleteItem(request):
+    try:
+        item = ItemModel.objects.filter(id__exact=request.POST['item'])[0]
+        item.use_yn = 'N'
+        item.save()
+        return HttpResponse("1")
+    except Exception as ex:
+        print('Error occured : ', ex)
+
+
+def deleteMart(request):
+    try:
+        mart = MartModel.objects.filter(id__exact=request.POST['mart'])[0]
+        mart.use_yn = 'N'
+        mart.save()
+        return HttpResponse("1")
+    except Exception as ex:
+        print('Error occured : ', ex)
