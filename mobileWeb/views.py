20dff38febc696fd207a9e6b07f643b12e703dee
90majh <90majh@gmail.com>
2020-02-24 10:42:32 +0900

    modify csrf exempt

--- mobileWeb/views.py
+++ mobileWeb/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .forms import *
 from datetime import datetime
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
@@ -88,6 +89,7 @@ def deleteMart(request):
     except Exception as ex:
         print('Error occured : ', ex)
 
+@csrf_exempt
 def purchaseItem(request):
     try:
         item = ItemModel.objects.filter(id__exact=request.POST['item'])[0]
