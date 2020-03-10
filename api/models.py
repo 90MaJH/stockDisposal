97b88ea08558da39e7a2ab12381bfbce69b310a0
90majh <90majh@gmail.com>
2020-03-10 15:01:02 +0900

    tmp commit

--- api/models.py
+++ api/models.py
@@ -9,6 +9,6 @@ from django.db import models
 class ItemModelTmp(models.Model):
     companyCode = models.CharField(blank=False, default='C0012', max_length=7)
     itemCode = models.CharField(blank=False, default='0001', max_length=7)
-    barcode = models.CharField(blank=False, default='123213 2823', max_length=20)
-    discountEndDttm = models.DateTimeField(blank=True, default='2020-03-28 23:00:00')
-    discountPrice = models.FloatField(blank=True)
+    barcode = models.CharField(blank=True, null=True, max_length=20)
+    discountEndDttm = models.DateTimeField(null=True)
+    discountPrice = models.FloatField(null=True)
